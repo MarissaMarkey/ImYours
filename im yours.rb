855dc28fade6ed4b95954223b6c8f428355de47b
MarissaMarkey <45051746+MarissaMarkey@users.noreply.github.com>
2020-01-21 11:37:36 -0800

    Add files via upload

---
+++ im yours.rb
@@ -0,0 +1,121 @@
+sample :ambi_lunar_land, rate:1.5
+sleep 4
+live_loop:other_loop do
+  use_synth:piano
+  play:B2, amp:0.5
+  sleep 0.25
+  play:Fs3, amp:0.5
+  sleep 0.25
+  play:B3, amp:0.5
+  sleep 0.25
+  play:Fs3, amp:0.5
+  sleep 0.25
+  play:B2, amp:0.5
+end
+live_loop:song do
+  sleep 0.5
+  play:B4, amp:5
+  sleep 0.25
+  play:Df5, amp:5
+  sleep 0.5
+  play:B4, amp:5
+  play:B2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:Fs3, amp:5
+  play:B3, amp:5
+  sleep 0.5
+  play:B2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:Fs3, amp:5
+  play:B3, amp:5
+  sleep 0.5
+  play:B2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:Fs3, amp:5
+  play:B3, amp:5
+  sleep 0.5
+  play:B2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.25
+  play:B4, amp:5
+  sleep 0.25
+  play:Df5, amp:5
+  sleep 0.5
+  play:Fs4, amp:5
+  play:Fs2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:Cs3, amp:5
+  play:Fs3, amp:5
+  sleep 0.5
+  play:Fs2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:Cs3, amp:5
+  play:Fs3, amp:5
+  sleep 0.5
+  play:Fs2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:Cs3, amp:5
+  play:Fs3, amp:5
+  sleep 0.5
+  play:Fs2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.25
+  play:Fs4, amp:5
+  sleep 0.25
+  play:Gs4, amp:5
+  sleep 0.5
+  play:Ds4, amp:5
+  play:G2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:Ds3, amp:5
+  play:Gs3, amp:5
+  sleep 0.5
+  play:G2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:Ds3, amp:5
+  play:Gs3, amp:5
+  sleep 0.5
+  play:G2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:Ds3, amp:5
+  play:Gs3, amp:5
+  sleep 0.5
+  play:G2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:Ds4, amp:5
+  sleep 0.25
+  play:Ds4, amp:5
+  sleep 0.5
+  play:E4, amp:5
+  play:E2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:B2, amp:5
+  play:E3, amp:5
+  sleep 0.5
+  play:E2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:B2, amp:5
+  play:E3, amp:5
+  sleep 0.5
+  play:E2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+  play:B2, amp:5
+  play:E3, amp:5
+  sleep 0.5
+  play:E2, amp:5
+  sample :drum_tom_mid_soft, rate:1.5
+  sleep 0.5
+end
